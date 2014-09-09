67bb253d15cf4f81f5a953193697da3b4adba5a1
Cameron Edwards <cameronprattedwards@gmail.com>
2014-09-09 14:26:02 -0400

    Implement UMD in services/validator

--- dist/schema-form.js
+++ dist/schema-form.js
@@ -910,65 +910,72 @@
   }]);
 }));
 
-/*  Common code for validating a value against its form and schema definition */
-/* global tv4 */
-angular.module('schemaForm').factory('sfValidator', [function() {
+(function(root, factory) {
+  if (typeof define == 'function' && define.amd)
+    define(['angular', 'angular-schema-form/module'], factory);
+  else
+    factory(angular);
+}(this, function(angular) {
+  /*  Common code for validating a value against its form and schema definition */
+  /* global tv4 */
+  angular.module('schemaForm').factory('sfValidator', [function() {
 
-  var validator = {};
+    var validator = {};
 
-  /**
-   * Validate a value against its form definition and schema.
-   * The value should either be of proper type or a string, some type
-   * coercion is applied.
-   *
-   * @param {Object} form A merged form definition, i.e. one with a schema.
-   * @param {Any} value the value to validate.
-   * @return a tv4js result object.
-   */
-  validator.validate = function(form, value) {
+    /**
+     * Validate a value against its form definition and schema.
+     * The value should either be of proper type or a string, some type
+     * coercion is applied.
+     *
+     * @param {Object} form A merged form definition, i.e. one with a schema.
+     * @param {Any} value the value to validate.
+     * @return a tv4js result object.
+     */
+    validator.validate = function(form, value) {
 
-    var schema = form.schema;
+      var schema = form.schema;
 
-    if (!schema) {
-      //Nothings to Validate
-      return value;
-    }
-
-    //Type cast and validate against schema.
-    //Basic types of json schema sans array and object
-    if (schema.type === 'integer') {
-      value = parseInt(value, 10);
-    } else if (schema.type === 'number') {
-      value = parseFloat(value, 10);
-    } else if (schema.type === 'boolean' && typeof value === 'string') {
-      if (value === 'true') {
-        value = true;
-      } else if (value === 'false') {
-        value = false;
+      if (!schema) {
+        //Nothings to Validate
+        return value;
       }
-    }
 
-    // Version 4 of JSON Schema has the required property not on the
-    // property itself but on the wrapping object. Since we like to test
-    // only this property we wrap it in a fake object.
-    var wrap = {type: 'object', 'properties': {}};
-    var propName = form.key[form.key.length - 1];
-    wrap.properties[propName] = schema;
+      //Type cast and validate against schema.
+      //Basic types of json schema sans array and object
+      if (schema.type === 'integer') {
+        value = parseInt(value, 10);
+      } else if (schema.type === 'number') {
+        value = parseFloat(value, 10);
+      } else if (schema.type === 'boolean' && typeof value === 'string') {
+        if (value === 'true') {
+          value = true;
+        } else if (value === 'false') {
+          value = false;
+        }
+      }
 
-    if (form.required) {
-      wrap.required = [propName];
-    }
-    var valueWrap = {};
-    if (angular.isDefined(value)) {
-      valueWrap[propName] = value;
-    }
+      // Version 4 of JSON Schema has the required property not on the
+      // property itself but on the wrapping object. Since we like to test
+      // only this property we wrap it in a fake object.
+      var wrap = {type: 'object', 'properties': {}};
+      var propName = form.key[form.key.length - 1];
+      wrap.properties[propName] = schema;
 
-    return tv4.validateResult(valueWrap, wrap);
+      if (form.required) {
+        wrap.required = [propName];
+      }
+      var valueWrap = {};
+      if (angular.isDefined(value)) {
+        valueWrap[propName] = value;
+      }
 
-  };
+      return tv4.validateResult(valueWrap, wrap);
 
-  return validator;
-}]);
+    };
+
+    return validator;
+  }]);
+}));
 
 /**
  * Directive that handles the model arrays
