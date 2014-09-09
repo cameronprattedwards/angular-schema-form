af700c53917b3aa1ac630f1a9336af0064cf296e
Cameron Edwards <cameronprattedwards@gmail.com>
2014-09-09 14:00:21 -0400

    Depend on module and indent

--- src/sfPath.js
+++ src/sfPath.js
@@ -1,33 +1,33 @@
 (function(root, factory) {
   if (typeof define == 'function' && define.amd)
-    define(['angular', 'ObjectPath'], factory);
+    define(['angular', 'angular-schema-form/module', 'ObjectPath'], factory);
   else
     factory(angular);
 }(this, function(angular) {
-angular.module('schemaForm').provider('sfPath',
-['ObjectPathProvider', function(ObjectPathProvider) {
-  var ObjectPath = {parse: ObjectPathProvider.parse};
+  angular.module('schemaForm').provider('sfPath',
+  ['ObjectPathProvider', function(ObjectPathProvider) {
+    var ObjectPath = {parse: ObjectPathProvider.parse};
 
-  // if we're on Angular 1.2.x, we need to continue using dot notation
-  if (angular.version.major === 1 && angular.version.minor < 3) {
-    ObjectPath.stringify = function(arr) {
-      return Array.isArray(arr) ? arr.join('.') : arr.toString();
+    // if we're on Angular 1.2.x, we need to continue using dot notation
+    if (angular.version.major === 1 && angular.version.minor < 3) {
+      ObjectPath.stringify = function(arr) {
+        return Array.isArray(arr) ? arr.join('.') : arr.toString();
+      };
+    } else {
+      ObjectPath.stringify = ObjectPathProvider.stringify;
+    }
+
+    // We want this to use whichever stringify method is defined above,
+    // so we have to copy the code here.
+    ObjectPath.normalize = function(data, quote) {
+      return ObjectPath.stringify(Array.isArray(data) ? data : ObjectPath.parse(data), quote);
     };
-  } else {
-    ObjectPath.stringify = ObjectPathProvider.stringify;
-  }
 
-  // We want this to use whichever stringify method is defined above,
-  // so we have to copy the code here.
-  ObjectPath.normalize = function(data, quote) {
-    return ObjectPath.stringify(Array.isArray(data) ? data : ObjectPath.parse(data), quote);
-  };
-
-  this.parse = ObjectPath.parse;
-  this.stringify = ObjectPath.stringify;
-  this.normalize = ObjectPath.normalize;
-  this.$get = function () {
-    return ObjectPath;
-  };
-}]);
+    this.parse = ObjectPath.parse;
+    this.stringify = ObjectPath.stringify;
+    this.normalize = ObjectPath.normalize;
+    this.$get = function () {
+      return ObjectPath;
+    };
+  }]);
 }));
